20b27fb8bdd4e557f0f23d390e43031f67fb2cfa
leep3 <leep3@oregonstate.edu>
2017-11-18 19:42:11 -0800

    edit feture
    same thing

---
+++ web/views/editResultTask.php
@@ -0,0 +1,56 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+  <title>Add Todo List - AceCollege</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../public/css/main.css">
+</head>
+<body>
+  <div id="site-header">
+    <div id="logo">
+      <h1>AceCollege</h1>
+    </div>
+    <div id="navbar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Todo List</a></li>
+        <li><a href="#">Study Group</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="site-content">
+	<?php
+
+		require_once '../app/init.php';
+
+// Assign the input to the proper variable.
+		$tID = mysqli_real_escape_string($conn, $_POST['tID']);
+		$title = mysqli_real_escape_string($conn, $_POST['title']);
+		$description = mysqli_real_escape_string($conn, $_POST['description']);
+		$dateAssigned = mysqli_real_escape_string($conn, $_POST['dateAssigned']);
+		$magnitude = mysqli_real_escape_string($conn, $_POST['magnitude']);
+		$completion = mysqli_real_escape_string($conn, $_POST['completion']);
+		$tag = mysqli_real_escape_string($conn, $_POST['tag']);
+
+
+// Open the table to insert
+
+		$query = "UPDATE Tasks SET Title = '$title', Description = '$description', DateAssigned = '$dateAssigned', Magnitude = '$magnitude', tag = '$tag' WHERE tID = '$tID';" ;
+
+		if(mysqli_query($conn, $query)){
+			echo "The task is updated.";
+		} else{
+			echo "ERROR: Could not able to execute $query. " . mysqli_error($conn);
+		}
+
+		echo "WORKING!!!";
+	?>
+  </div>
+
+  <div id="site-footer">
+
+  </div>
+</body>
+</html>
+
---
+++ web/views/editTask.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Add Todo List - AceCollege</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../public/css/main.css">
+</head>
+<body>
+  <div id="site-header">
+    <div id="logo">
+      <h1>AceCollege</h1>
+    </div>
+    <div id="navbar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Todo List</a></li>
+        <li><a href="#">Study Group</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="site-content">
+    <form action="editResultTask.php" method="post">
+    <p>
+        <label for="tID">Type the tID that you want to change: </label>
+        <input type="text" name="tID" id="tID">
+	<p>
+        <label for="title">Title: </label>
+        <input type="text" name="title" id="title">
+    </p>
+    <p>
+        <label for="description">Description:</label>
+        <input type="text" name="description" id="description">
+    </p>
+
+    <p>
+        <label for="dateAssigned">Date Assigned:</label>
+        <input type="text" name="dateAssigned" id="dateAssigned">
+    </p>
+    <p>
+        <label for="magnitude">Magnitude:</label>
+        <input type="text" name="magnitude" id="magnitude">
+    </p>
+    <p>
+        <label for="tag">Tag: </label>
+        <input type="text" name="tag" id="tag">
+    </p>
+	<p>
+        <label for="tag">Completion: </label>
+        <input type="text" name="completion" id="completion">
+    </p>
+    </p>
+
+    <input type="submit" value="Submit">
+
+    <br></br>
+
+</form>
+
+  </div>
+
+  <div id="site-footer">
+
+  </div>
+</body>
+</html>
