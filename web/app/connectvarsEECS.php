c8924b7d5f463cccfaceedc6cc70c19dd8e829f6
Mazen <mazen.ota@gmail.com>
2020-05-16 20:23:16 -0700

    Delete connectvarsEECS.php

--- web/app/connectvarsEECS.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_alotaima');
-  define('DB_PASSWORD', '7758');
-  define('DB_NAME', 'cs340_alotaima');
-?>
